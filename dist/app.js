6a628c9e628b97df3953f38cd2deed306807df3f
Burhan <burhan.hasan.dev@gmail.com>
2017-10-20 21:43:47 +0400

    Added datasource add functionality

--- dist/app.js
+++ dist/app.js
@@ -11,25 +11,36 @@ var cats = [
     , { id: 8, pid: 2, name: '650' }
 ];
 
-function ComponentTreeView(element, data) {
+function ComponentTreeView(options) {
+    var _prefix = options.element.attr('id') + '-';
+    var _tempActiveItem = null;
+
+    var getElementById = function (id) {
+        for (var i = 0; i < options.data.length; ++i) {
+            var cur = options.data[i];
+            if (cur.id == id)
+                return cur;
+        }
+    }
+
     var getIconCode = function (icoName) {
         var em = document.createElement('em');
         em.className = 'icon-' + icoName;
         return em;
     }
 
-    var funcGenerateDOM = function (pid, data) {
+    var funcGenerateDOM = function (pid) {
         var docFragmentList = document.createDocumentFragment();
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].pid == pid) {
+        for (var i = 0; i < options.data.length; i++) {
+            if (options.data[i].pid == pid) {
                 var li = document.createElement('li');
                 var span = document.createElement('span');
-                span.textContent = data[i].name;
-                li.id = data[i].id;
+                span.textContent = options.data[i].name;
+                li.id = _prefix + options.data[i].id;
                 li.appendChild(span);
 
                 docFragmentList.appendChild(li);
-                var subItems = funcGenerateDOM(data[i].id, data);
+                var subItems = funcGenerateDOM(options.data[i].id, options.data);
                 if (subItems) {
                     var ul = document.createElement('ul');
                     ul.appendChild(subItems);
@@ -42,24 +53,53 @@ function ComponentTreeView(element, data) {
         return docFragmentList.childElementCount ? docFragmentList : null;
     }
 
-    element.append($(funcGenerateDOM(0, data)));
+    options.element.append($(funcGenerateDOM(0, options.data)));
 
-    element.on('click', function (e) {
+    options.element.on('click', function (e) {
         var $target = $(e.target);
 
-        element.find('.--active').removeClass('--active');
 
         if ($target.closest('li').length) {
             var parentLi = $target.closest('li');
+            options.element.find('.--active').removeClass('--active');
             parentLi.addClass('--active');
             if (parentLi.find('ul').length)
                 parentLi.toggleClass('--opened');
         }
         else if ($target.closest('button').length) {
             var btn = $target.closest('button');
+            if (btn.hasClass('component-treeview-options-save')) {
+                var activeItem = options.element.find('.--active');
+                var activeItemId = activeItem.attr('id');
+                var activeItemDataId = activeItemId.slice(-(activeItemId.length - activeItemId.indexOf('-') - 1));
+                _tempActiveItem = getElementById(activeItemDataId);
 
+                var activeItemObj = {
+                    elem: activeItem,
+                    id: activeItemId,
+                    displayName: activeItem.text(),
+                    object: _tempActiveItem
+                }
+                options.onAdd(activeItemObj);
+                _tempActiveItem = null;
+            }
         }
     });
+
+    this.dataSource = {
+        add: function (input) {
+            console.log('----')
+            console.log(_tempActiveItem)
+            console.log(input)
+            console.log('----')
+        }
+    }
 }
 
-var componentTreeView = new ComponentTreeView($('#mytreeview'), cats);
+var componentTreeView = new ComponentTreeView({
+    element: $('#mytreeview'),
+    data: cats,
+    onAdd: function () {
+        componentTreeView.dataSource.add({ name: 'Salam' })
+    }
+});
